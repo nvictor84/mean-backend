01c1573887ee48180c789c4c2421a9355b9c4432
VICTOR NOGUERA <noguera.victor@gmail.com>
2018-12-22 19:35:49 -0500

    Initial Commit

---
+++ routes/posts.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var router = express.Router();
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/mean-stack", { useNewUrlParser: true })
+    .then(() => {
+        console.info('Connected to mean-mongo');
+    })
+    .catch(err => {
+        console.error(err.message);
+    });
+
+
+const Post = require('../models/post');
+
+/* GET posts */
+router.get('/', (req, res, next) => {
+    Post.find()
+        .then(docs => {
+            res.status(200).json({
+                success: true,
+                posts: docs.map(doc => {
+                    return {
+                        id: doc._id,
+                        title: doc.title,
+                        content: doc.content
+                    }
+                })
+            });
+        })
+        .catch(err => {
+            res.status(400).json({
+                success: false,
+                message: err.message
+            });
+        });
+});
+
+/* POST posts */
+router.post('/', (req, res, next) => {
+        if (!req.body.newPost) {
+            res.status(203).json({
+                success: false,
+                message: `New post missing, nothing inserted`
+            })
+        } else {
+            const newPost = new Post(req.body.newPost);
+            newPost.save()
+                .then(result => {
+                    res.status(200).json({
+                        success: true
+                    });
+                });
+        }
+});
+
+/* DELETE posts */
+router.delete('/:id', (req, res, next) => {
+        if (!req.params.id) {
+            res.status(203).json({
+                success: false,
+                message: `Missing post id. Nothing to delete.`
+            })
+        } else {
+            Post.deleteOne({ _id: req.params.id })
+                .then(result => {
+                    res.status(200).json({
+                        success: true,
+                        message: `Post ${req.params.id} deleted!`
+                    });
+                })
+                .catch(err => {
+                    res.status(500).json({
+                        success: false,
+                        message: `Post ${req.params.id} NOT deleted!`
+                    });
+                })
+        }
+});
+
+module.exports = router;
